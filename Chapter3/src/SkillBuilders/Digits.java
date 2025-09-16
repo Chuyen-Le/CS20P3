729e0171a1a0e1c05335710ac6177eadb4bd2a84
1100121756 <1100121756@L815WD220033.edu.cbe.ab.ca>
2025-09-16 14:52:46 -0600

    2:52 pm

---
+++ Chapter3/src/SkillBuilders/Digits.java
@@ -0,0 +1,19 @@
+package SkillBuilders;
+
+import java.util.Scanner;
+
+public class Digits {
+
+	public static void main(String[] args) 
+	{
+		// Ask user to enter a two-digit number
+		Scanner scanner = new Scanner(System.in);
+		System.out.print("Enter a two-digit number: ");
+		int number = scanner.nextInt();
+		
+		// Prompt the ones-place and tens-place digits
+		System.out.println("The ones-place digits is: " + number % 10);
+		System.out.println("The tens-place digits is: " + (int)number / 10);
+	}
+
+}
