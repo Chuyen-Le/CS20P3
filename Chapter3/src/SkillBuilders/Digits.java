ca973297a46c93af652074ea84ec377013122480
1100121756 <1100121756@L815WD220033.edu.cbe.ab.ca>
2025-09-18 14:22:47 -0600

    2:22 pm

--- Chapter3/src/SkillBuilders/Digits.java
+++ Chapter3/src/SkillBuilders/Digits.java
@@ -11,9 +11,13 @@ public class Digits {
 		System.out.print("Enter a two-digit number: ");
 		int number = scanner.nextInt();
 		
+		// Find the ones and tens
+		int Ones = number % 10;
+		int Tens = number / 10;
+		
 		// Prompt the ones-place and tens-place digits
-		System.out.println("The ones-place digits is: " + number % 10);
-		System.out.println("The tens-place digits is: " + (int)number / 10);
+		System.out.println("The ones-place digits is: " + Ones);
+		System.out.println("The tens-place digits is: " + Tens);
 	}
 
 }
