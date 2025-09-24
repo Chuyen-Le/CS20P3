39e8f99c0227311bfe96d49b0dbe6561f0e1af2f
1100121756 <1100121756@L815WD220033.edu.cbe.ab.ca>
2025-09-24 12:56:05 -0600

    12:56 pm

---
+++ Chapter4/src/SkillBuilders/Delivery.java
@@ -0,0 +1,29 @@
+package SkillBuilders;
+
+import java.util.Scanner;
+
+public class Delivery 
+{
+
+	public static void main(String[] args) 
+	{
+		// Ask the user about the size of a package
+		Scanner scanner = new Scanner(System.in);
+		
+		System.out.print("What is the length of your package: ");
+		double length = scanner.nextDouble();
+		
+		System.out.print("What is the width of your package: ");
+		double width = scanner.nextDouble();
+		
+		System.out.print("What is the height of your package: ");
+		double height = scanner.nextDouble();
+
+		if (length <= 10 && width <= 10 && height <= 10) {
+			System.out.print("Accept");
+		} else {
+			System.out.print("Reject");
+		}
+	}
+
+}
