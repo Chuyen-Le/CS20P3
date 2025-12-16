53316e314ecfa7fa9d81d6ba8b67994ccf27b527
1100121756 <1100121756@L815WD220033.edu.cbe.ab.ca>
2025-12-16 14:42:21 -0700

    2:42 pm

--- Chapter9/src/Mastery/e5.java
+++ Chapter9/src/Mastery/e5.java
@@ -1,3 +1,16 @@
+/*
+
+Name: Minh Chuyen Le
+Class: Computer Science 20 P3
+
+Requirements: 
+The game of Mastermind is played as follows: One player (the code maker) chooses a secret arrangement of colored pegs and the other player (the code breaker) tries to guess it. For each guess, the code
+breaker puts forth an arrangement of colored pegs, and the code maker reports two numbers:
+1. The number of pegs that are the correct color and in the correct position.
+2. The number of pegs that are the correct color regardless of whether they are in the correct position
+
+*/
+
 package Mastery;
 
 import java.util.Scanner;
@@ -9,36 +22,41 @@ public class e5
 	{
 		Scanner scanner = new Scanner(System.in);
 		
-		// Set the number of pegs
+		// the number of pegs
 		System.out.print("Enter the number of pegs <1-10>: ");				
 		int numPeg = scanner.nextInt();
 		
-		// set the number of colors
+		// the number of possible colors
 		System.out.print("Enter the number of colors <1-9>: ");				
 		int numColor = scanner.nextInt();
 		
-		int[] correctAns = new int[numPeg];		// an array for correct answer
-		int[] userGuess = new int[numPeg];		// an array for user's answer
+		int[] correctAns = new int[numPeg];		// array stores correct answer 
+		int[] userGuess = new int[numPeg];		// array stores the user's guess
 		
 		Random rand = new Random();
 		
-		// set the values for correct answers
+		// set up random colors for each peg
 		for (int i = 0; i < numPeg; i++)									
 		{
 			correctAns[i] = rand.nextInt(numColor) + 1; 
 		}
 		
-		// the number of trials
-		int numTrial = 1;													
+		// counter to keep track of how many guesses the user has made
+		int numTrial = 1;	
+		
+		// main game loop
 		while (true)
 		{
 			System.out.println("\nGuess: " + numTrial);
-			int countPeg = 0;
-			int countColor = 0;
+			int countPeg = 0;					// count of pegs that are correct in both color and position
+			int countColor = 0;					// count of pegs that have the correct color but wrong position
 			
-			boolean[] correctPegUsed = new boolean[numPeg];					
+			// boolean array to mark which pegs are already matched
+			boolean[] correctPegUsed = new boolean[numPeg];	
+			// boolean array to mark which pegs in the user's guess are matched
 			boolean[] guessColorUsed = new boolean[numPeg];
 			
+			// the user's guess and check for correct peg and position
 			for (int i = 0; i < numPeg; i++)
 			{
 				System.out.printf("Color for peg %d: ", i+1);
@@ -47,8 +65,7 @@ public class e5
 				
 				if (ans == correctAns[i])
 				{
-					// if the user's answer is correct, 
-					// update the number of correct guesses
+					// if the user's answer is correct, update the number of correct guesses
 					// assign the boolean of this index is true
 					countPeg++;
 					correctPegUsed[i] = true;							
@@ -56,16 +73,17 @@ public class e5
 				}
 			}
 			
+			// check for correct colors in the wrong positions
 			for (int i = 0; i < numPeg; i++) 
 			{
-				// check the index that the boolean is false
+				// check the index in the boolean is false
 			    if (!guessColorUsed[i]) 
 			    {
 			        for (int j = 0; j < numPeg; j++) 
 			        {
 			            if (!correctPegUsed[j] && (userGuess[i] == correctAns[j])) 
 			            {
-			            	// update how many color user guesses correctly
+			            	// update how many color the user guesses correctly
 			            	countColor++;
 			                correctPegUsed[j] = true;		// assign the boolean of this index is true
 			                break;
@@ -74,14 +92,14 @@ public class e5
 			    }
 			}
 			
-			// if the user guesses correctly all numbers, it prompts out a line and stop the program.
+			// if all guesses are correct, the user wins and the game stops
 			if (countPeg == numPeg)
 			{
 				System.out.print("\nYou have broken the code in " + numTrial + " guesses");
 				break;
 			}
 			
-			// if the user don't get all correct answers, it prompts out a line and continue the while loop
+			// if the user don't get all correct answers, prompts out feedback and continue the while loop
 			System.out.println("You have " + countPeg + " peg<s> correct and " + countColor + " color<s> correct.");
 			
 			// update the number of trials
@@ -91,3 +109,34 @@ public class e5
 		scanner.close();
 	}
 }
+
+//Test case
+/*
+Enter the number of pegs <1-10>: 3
+Enter the number of colors <1-9>: 5
+
+Guess: 1
+Color for peg 1: 1
+Color for peg 2: 2
+Color for peg 3: 3
+You have 2 peg<s> correct and 0 color<s> correct.
+
+Guess: 2
+Color for peg 1: 1
+Color for peg 2: 2
+Color for peg 3: 5
+You have 1 peg<s> correct and 0 color<s> correct.
+
+Guess: 3
+Color for peg 1: 4
+Color for peg 2: 2
+Color for peg 3: 3
+You have 2 peg<s> correct and 0 color<s> correct.
+
+Guess: 4
+Color for peg 1: 3
+Color for peg 2: 2
+Color for peg 3: 3
+
+You have broken the code in 4 guesses
+*/
